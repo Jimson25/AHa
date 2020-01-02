03893b2e15f2260ca06c6cbc712f4623f4a3a7b5
jinaosong <248578737@qq.com>
2020-01-02 14:45:41 +0800

    '添加文件重命名demo'

---
+++ src/demo/_0101/RenameFile.java
@@ -0,0 +1,55 @@
+package demo._0101;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RenameFile {
+    static String srcPath;
+
+    public static void main(String[] args) {
+        System.out.println("请输入文件夹路径:");
+        srcPath = input();
+        moveFile(srcPath);
+//        renameFile(input);
+    }
+
+    /**
+     * 将文件移动到输入的目录
+     */
+    public static void moveFile(String path) {
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                moveFile(f.getAbsolutePath());
+            } else {
+                f.renameTo(new File(srcPath + "/" + f.getName()));
+            }
+        }
+    }
+
+    /**
+     * 将文件重命名在原路径
+     *
+     * @param path
+     */
+    public static void renameFile(String path) {
+        File[] files = new File(path).listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                renameFile(file.getAbsolutePath());
+            } else {
+                String name = file.getName();
+                System.out.printf("原文件名: %s \n", name);
+                int indexOf = name.indexOf('.') + 1;
+                String finalName = name.substring(indexOf);
+                file.renameTo(new File(path + "/" + finalName));
+            }
+        }
+    }
+
+    public static String input() {
+        return new Scanner(System.in).nextLine();
+    }
+}
